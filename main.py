e9d3011167a70ddfacb1f1a78efe93032215da52
Sukkrishvar Vijay <88849081+sukkrishvar-vijay@users.noreply.github.com>
2023-04-10 19:02:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -132,3 +132,18 @@ class QuizBrain:
         print(f"The correct answer was: {correct_answer}.")
         print(f"Your current score is: {self.score}/{self.question_number}")
         print("\n")
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
